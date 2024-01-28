e08901fb3b41ff40a40edc71bf866b015e057ded
CalebRichardson1 <crichnumber2@gmail.com>
2024-01-27 19:04:22 -0600

    Removed worthless particles, and added player spawn points

--- Assets/_Scripts/ScriptableObjects/LevelSpawnPoints.cs
+++ Assets/_Scripts/ScriptableObjects/LevelSpawnPoints.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Level Spawn List", fileName = "NewLevelSpawn")]
 public class LevelSpawnPoints : ScriptableObject{
 	public string LevelName;
+	public float LevelTime;
 	public List<Vector3> spawnPointsLocation = new List<Vector3>();
 }
